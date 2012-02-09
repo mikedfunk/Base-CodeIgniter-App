8d582623e737b73dae20a4b0b22bff80dba9fb56
Mike Funk <mfunk@xulonpress.com>
2012-02-08 19:30:45 -0500

    set up unit testing, htaccess.

---
+++ tests/assets/scripts/test_scripts.js
@@ -0,0 +1,99 @@
+/**
+ * test_scripts
+ * 
+ * Description
+ * 
+ * @license		Copyright Xulon Press, Inc. All Rights Reserved.
+ * @author		Xulon Press
+ * @link		http://xulonpress.com
+ * @email		info@xulonpress.com
+ * 
+ * @file		test_scripts.js
+ * @version		1.0
+ * @date		02/07/2012
+ * 
+ * Copyright (c) 2012
+ */
+
+// --------------------------------------------------------------------------
+
+/**
+ * document ready
+ */
+$(function()
+{
+
+	// --------------------------------------------------------------------------
+	
+	/**
+	 * test example
+	 */
+	module ('', {
+		setup: function()
+		{
+			// setup
+			var the_html = '';
+			$('#qunit-fixture').html(the_html);
+		},
+		teardown: function()
+		{
+			$('#qunit-fixture').empty();
+		}
+	});
+	
+	test('function_name()', function()
+	{
+		expect(5);
+		
+		// test
+	});
+
+
+// 	// --------------------------------------------------------------------------
+// 	
+// 	/**
+// 	 * examples
+// 	 */
+// 	test("a basic test example", function() 
+// 	{
+// 		ok( true, "this test is fine" );
+// 		var value = "hello";
+// 		equal( value, "hello", "We expect value to be hello" );
+// 	});
+// 	
+// 	// --------------------------------------------------------------------------
+// 	
+// 	module("Module A");
+// 	
+// 	// --------------------------------------------------------------------------
+// 	
+// 	test("first test within module", function() 
+// 	{
+// 		ok( true, "all pass" );
+// 	});
+// 	
+// 	// --------------------------------------------------------------------------
+// 	
+// 	test("second test within module", function() 
+// 	{
+// 		ok( true, "all pass" );
+// 	});
+// 	
+// 	// --------------------------------------------------------------------------
+// 	
+// 	module("Module B");
+// 	
+// 	// --------------------------------------------------------------------------
+// 	
+// 	test("some other test", function() 
+// 	{
+// 		expect(2);
+// 		equal( true, false, "failing test" );
+// 		equal( true, true, "passing test" );
+// 	});
+// 	
+// 	// --------------------------------------------------------------------------
+	
+});
+/* End of file test_scripts.js */
+/* Location: ./xulonpress_website/ci_pages/tests/assets/scripts/test_scripts.js */
