2f18c5ec61d765265f65679559be15c580a9d1f6
Mike Funk <mfunk@xulonpress.com>
2012-02-08 22:21:34 -0500

    added gitignore

---
+++ assets/scripts/actions.js
@@ -0,0 +1,29 @@
+/**
+ * actions.js
+ * 
+ * Description
+ * 
+ * @license		Copyright Xulon Press, Inc. All Rights Reserved.
+ * @author		Xulon Press
+ * @link		http://xulonpress.com
+ * @email		info@xulonpress.com
+ * 
+ * @file		actions.js
+ * @version		1.0
+ * @date		02/08/2012
+ * 
+ * Copyright (c) 2012
+ */
+
+// --------------------------------------------------------------------------
+
+/**
+ * document ready
+ */
+$(function()
+{
+	
+});
+
+/* End of file actions.js */
+/* Location: ./bookymark/assets/scripts/actions.js */
---
+++ assets/scripts/scripts.js
@@ -0,0 +1,22 @@
+/**
+ * scripts.js
+ * 
+ * Description
+ * 
+ * @license		Copyright Xulon Press, Inc. All Rights Reserved.
+ * @author		Xulon Press
+ * @link		http://xulonpress.com
+ * @email		info@xulonpress.com
+ * 
+ * @file		scripts.js
+ * @version		1.0
+ * @date		02/08/2012
+ * 
+ * Copyright (c) 2012
+ */
+
+// --------------------------------------------------------------------------
+
+
+/* End of file scripts.js */
+/* Location: ./bookymark/assets/scripts/scripts.js */
