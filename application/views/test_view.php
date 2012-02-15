9afe2313a09c7437219bbeb6d99162bdc0c21010
Mike Funk <mfunk@xulonpress.com>
2012-02-15 14:47:49 -0500

    updated welcome page markup.

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -24,10 +24,10 @@
 <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<p><code>application/views/welcome_message.php</code></p>
 
 		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
+		<p><code>application/controllers/welcome.php</code></p>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 		
