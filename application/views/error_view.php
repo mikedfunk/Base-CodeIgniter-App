44870223ec16b3ca1540bd226e3a003c0cf78266
Mike Funk <mfunk@xulonpress.com>
2012-02-18 02:19:39 -0500

    updated error views.

---
+++ application/views/error_view.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * error_view
+ * 
+ * Description
+ * 
+ * @license		Copyright Xulon Press, Inc. All Rights Reserved.
+ * @author		Xulon Press
+ * @link		http://xulonpress.com
+ * @email		info@xulonpress.com
+ * 
+ * @file		error_view.php
+ * @version		1.0
+ * @date		02/18/2012
+ * 
+ * Copyright (c) 2012
+ */
+?>
+
+<div class="container">
+<div class="page-header">
+<h1><?=$title?></h1>
+</div><!--page-header-->
+<p><?=$message?></p>
+</div><!--container-->
+
+<?php
+/* End of file error_view.php */
+/* Location: ./base_codeigniter_app/application/views/error_view.php */
