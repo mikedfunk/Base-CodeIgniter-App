6e1397125c93ad285604e80ee3d688cfeb4f9fce
Mike Funk <mfunk@xulonpress.com>
2012-02-26 19:40:53 -0500

    updated MY_Router with docblock.

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -1,6 +1,31 @@
 <?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+/**
+ * my_router
+ * 
+ * extends router to load HMVC mx router class.
+ * 
+ * @license		http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @author		Calvin Froedge
+ * @link		http://www.calvinfroedge.com
+ * 
+ * @file		MY_Router.php
+ * @version		1.0
+ * @date		02/26/2012
+ * 
+ * Copyright (c) 2012
+ */
+ 
+// --------------------------------------------------------------------------
 
-/* load the MX_Router class */
-require APPPATH."third_party/MX/Router.php";
+require FCPATH_U.APPPATH_U."third_party/MX/Router.php";
 
-class MY_Router extends MX_Router {}
+/**
+ * MY_Router class.
+ * 
+ * @extends MX_Router
+ */
+class MY_Router extends MX_Router 
+{
+}
+/* End of file MY_Router.php */
+/* Location: ./bookymark/application/core/MY_Router.php */
