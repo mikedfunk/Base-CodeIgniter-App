624f99594d678165d66161225c1e90fd9702dbec
Mike Funk <mfunk@christianpublishing.com>
2012-10-10 16:30:05 -0400

    whitespace

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -11,7 +11,7 @@
 
 /**
  * Welcome class.
- * 
+ *
  * @extends MY_Controller
  */
 class Welcome extends MY_Controller {
@@ -21,10 +21,10 @@ class Welcome extends MY_Controller {
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
